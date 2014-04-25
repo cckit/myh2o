4b79aba638997aeccb889ddda1dbaf06cbd5d9b5
Chun Kit Chan <tommychan19910123@gmail.com>
2014-04-25 16:37:25 -0400

    Move headers to template php
    Usage: <?php include 'theme/nav_fixed.php'; ?>
    <?php include 'theme/nav_bar.php'; ?>

---
+++ theme/nav_bar.php
@@ -0,0 +1,46 @@
+<div class="navbar-wrapper">
+    <div class="container">
+        <div class="navbar navbar-inverse">
+
+            <div class="navbar-header">
+                <a class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <a id="homeButton" class="navbar-brand" href="#">
+                    <span>MyH<sub>2</sub>O</span>
+                </a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="project.php">Project</a>
+                    </li>
+                    <li><a href="map.php">Live Map</a>
+                    </li>
+                    <li><a href="#">Charts</a>
+                    </li>
+                    <li><a href="query.php">Upload Data</a>
+                    </li>
+                    <li><a href="about.php">About us</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" role="button" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> Admin <span class="caret"></span></a>
+                        <ul id="g-account-menu" class="dropdown-menu" role="menu">
+                            <li><a href="#">My Profile</a>
+                            </li>
+                            <li><a href="#">My Submission</a>
+                            </li>
+                            <li><a href="#"><i class="glyphicon glyphicon-lock"></i> Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /container -->
+</div>
+<!-- /navbar wrapper -->
