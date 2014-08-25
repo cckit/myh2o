cf0234fdb2ec5aa3d620ba176ac7c0edaaee0fed
Chun Kit Chan <tommychan19910123@gmail.com>
2014-08-26 03:45:00 +0800

    Fix for initialization error

--- js/load_data.js
+++ js/load_data.js
@@ -14,7 +14,9 @@ var ac;
 
 $(document).ready(function () {
 	map_baidu = new BMap.Map("map-canvas");
-	map_baidu.centerAndZoom("中國", 4);
+	var point = new BMap.Point(104.114129,37.550339);
+  map_baidu.centerAndZoom(point, 4);
+
 	map_baidu.addControl(new BMap.NavigationControl());
 	map_baidu.addControl(new BMap.ScaleControl());
 	map_baidu.addControl(new BMap.OverviewMapControl());
@@ -133,11 +135,9 @@ var initialize = function initialize() {
     }(markers[i]));*/
 
 	var infoWindow_baidu = new BMap.InfoWindow(content);
-	markers[i].addEventListener("click", function(){
-      this.openInfoWindow(infoWindow_baidu);
-      var markerClusterer = new BMapLib.MarkerClusterer(map_baidu, {markers:markers});
-    });
+	markers[i].addEventListener("click", function(){this.openInfoWindow(infoWindow_baidu);});
   }
+  var markerClusterer = new BMapLib.MarkerClusterer(map_baidu, {markers:markers});
 }
 
 
