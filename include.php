439c919a085a2a676718d788a8fc63b15ffdff9f
Chun Kit Chan <tommychan19910123@gmail.com>
2014-08-25 09:27:09 +0800

    Change to local asset

--- include.php
+++ include.php
@@ -2,12 +2,12 @@
 
 <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+<link rel="stylesheet" href="asset/lib/css/bootstrap-3.2.0.min.css" />
+<link rel="stylesheet" href="asset/lib/css/jquery-ui-1.11.1.min.css" />
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="asset/lib/js/jquery-1.11.1.min.js"></script>
+<script src="asset/lib/js/jquery-ui-1.11.1.min.js"></script>
+<script src="asset/lib/js/bootstrap-3.2.0.min.js"></script>
 <script src="https://jquery-csv.googlecode.com/files/jquery.csv-0.71.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 
