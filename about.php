b6e7085c73a2ccdba3820298224a793fca4578d6
Chun Kit Chan <tommychan19910123@gmail.com>
2014-09-01 09:12:15 +0800

    Remove include.php

--- about.php
+++ about.php
@@ -1,76 +1,53 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>MyH2O - About</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="asset/lib/css/bootstrap-3.2.0.min.css" />
+        <link rel="stylesheet" href="css/theme.css">
 
-<head>
-	<title>MyH2O - Home</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-    <!-- Inlcuded files -->
-	<?php include('include.php'); ?>
-	<link href="css/theme.css" rel="stylesheet">
-</head>
-<body>
-
-    <?php include 'theme/nav_fixed.php' ?>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header"> Project Introduction
-                </h1>
-            </div>
-        </div>
-        <p style="font-size:18px">
-			China has long been troubled by severe water contamination. According to the Ministry of Environmental Protection, 280 million people use unsafe drinking water. Half of the urban drinking water supplies do not meet national standards. Government is currently in a transition period to make water quality information more public, but most communities are still left in the dark about the quality of the water they are exposed to. 
-		</p>
-		<br>
-		<p style="font-size:18px">
-			"MyH2O" is created to help Chinese people understand better their drinking water and associated health risks. We gather credible water quality info from published research, government monitoring agencies and university labs. We stimulate local water testing campaigns by distributing affordable water quality testing kits to university students, NGOs and community volunteers. We aims to make water quality accessible to people at all age, and advocate for clean potable water for China with the power of crowd sourcing and community engagement.<br>
-		</p>
-		<br>
-		<p style="font-size:18px">
-			We believe 1 + 1 = 11. The passion, dedication and generosity of resources from our supporters and collaborators are crucial to make this happen:
-		</p>
-        <blockquote>
+        <script src="asset/lib/js/jquery-1.11.1.min.js"></script>
+        <script src="asset/lib/js/bootstrap-3.2.0.min.js"></script>
+    </head>
+    <body>
+        <?php include 'theme/nav_fixed.php' ?>
+        <div class="container">
             <div class="row">
-                <div class="col-md-2"><b>Government</b></div>
-                <div class="col-md-10">Beijing Municipal Health Bureau</div>
-                <div class="col-md-12"></div>
-                <div class="col-md-2"><b>NGO</b></div>
-                <div class="col-md-10">China Youth Climate Action Network<br>
-            Alashan SEE Ecological Association<br>
-            Greenovation<br></div>
-                <div class="col-md-2"><b>Academia</b></div>
-                <div class="col-md-10">Tsinghua University, School of Environment<br>
-              Nankai University, College of Environmental Science and Engineering<br></div>
+                <div class="col-lg-12">
+                    <h1 class="page-header"> Project Introduction
+                    </h1>
+                </div>
             </div>
-        </blockquote>
-        <hr class="featurette-divider">
-
-        <!--<div class="featurette">
-            <img class="featurette-image img-circle pull-right" src="http://placehold.it/512">
-            <h2 class="featurette-heading">YOU are not alone!<span class="text-muted"></span></h2>
-            <p class="lead">According to a survey conducted by China Times, <br>78% worries about tap water safety, <br>90% says they know nothing about the water they drink. <br>Are you one of them?</p>
+            <p style="font-size:18px">
+                China has long been troubled by severe water contamination. According to the Ministry of Environmental Protection, 280 million people use unsafe drinking water. Half of the urban drinking water supplies do not meet national standards. Government is currently in a transition period to make water quality information more public, but most communities are still left in the dark about the quality of the water they are exposed to. 
+            </p>
+            <br>
+            <p style="font-size:18px">
+                "MyH2O" is created to help Chinese people understand better their drinking water and associated health risks. We gather credible water quality info from published research, government monitoring agencies and university labs. We stimulate local water testing campaigns by distributing affordable water quality testing kits to university students, NGOs and community volunteers. We aims to make water quality accessible to people at all age, and advocate for clean potable water for China with the power of crowd sourcing and community engagement.<br>
+            </p>
+            <br>
+            <p style="font-size:18px">
+                We believe 1 + 1 = 11. The passion, dedication and generosity of resources from our supporters and collaborators are crucial to make this happen:
+            </p>
+            <blockquote>
+                <div class="row">
+                    <div class="col-md-2"><b>Government</b></div>
+                    <div class="col-md-10">Beijing Municipal Health Bureau</div>
+                    <div class="col-md-12"></div>
+                    <div class="col-md-2"><b>NGO</b></div>
+                    <div class="col-md-10">China Youth Climate Action Network<br>
+                        Alashan SEE Ecological Association<br>
+                        Greenovation<br>
+                    </div>
+                    <div class="col-md-2"><b>Academia</b></div>
+                    <div class="col-md-10">Tsinghua University, School of Environment<br>
+                        Nankai University, College of Environmental Science and Engineering<br>
+                    </div>
+                </div>
+            </blockquote>
+            <hr class="featurette-divider">
         </div>
-
-        <hr class="featurette-divider">
-
-        <div class="featurette">
-            <img class="featurette-image img-circle pull-left" src="http://placehold.it/512">
-            <h2 class="featurette-heading">Reliable & Updated?<span class="text-muted"> Yes!</span></h2>
-            <p class="lead">We only take data from credible sources. Thousands of data are collected from grassroot water testing campaigns, high-profile research and national water surveys. All the information presented here are constantly verified and updated. </p>
-        </div>
-
-        <hr class="featurette-divider">
-
-        <div class="featurette">
-            <img class="featurette-image img-circle pull-right" src="http://placehold.it/512">
-            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Flatness.</span></h2>
-            <p class="lead">A big design trend for 2013 is "flat" design. Gone are the days of excessive gradients and shadows. Designers are producing cleaner flat designs, and Bootstrap 3 takes advantage of this minimalist trend.</p>
-        </div>
-
-        <hr class="featurette-divider">-->
-	</div>
-</body>
+    </body>
 </html>
-
